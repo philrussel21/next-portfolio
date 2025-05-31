cf4ad2c1362c727063de29f4245c514132846269
Phil Antiporda <phil.antiporda21@gmail.com>
2025-05-31 20:43:31 +1000

    chore: init contact form details submission

--- components/organisms/contact-form.tsx
+++ components/organisms/contact-form.tsx
@@ -1,17 +1,18 @@
 "use client"
 
-import {useFormState} from "react-dom"
+import {useFormState, useFormStatus} from "react-dom"
 import {Card, CardTitle, CardHeader, CardDescription, CardContent, CardFooter} from "../ui/card"
 import {Input} from "../ui/input"
 import {Label} from "../ui/label"
 import {Button} from "../ui/button"
 import {Textarea} from "../ui/textarea"
 
-import {CheckIcon} from "@phosphor-icons/react/dist/ssr"
+import {CheckCircleIcon} from "@phosphor-icons/react/dist/ssr"
 import {contactFormAction} from "@app/lib/actions"
+import {Fragment} from "react"
 
-const ContactForm = () => {
-	const [state, formAction, pending] = useFormState(contactFormAction, {
+const ContactFormContent = () => {
+	const [state, formAction] = useFormState(contactFormAction, {
 		defaultValues: {
 			name: "",
 			email: "",
@@ -20,92 +21,110 @@ const ContactForm = () => {
 		success: false,
 		errors: null,
 	})
+	const {pending} = useFormStatus();
 
 	return (
-		<Card className="w-full max-w-lg mx-auto">
-			<CardHeader className="text-center">
-				<CardTitle>Let's build something great together.</CardTitle>
-				<CardDescription>
-					I'd love to hear about your project! Share your details, and we'll discuss how to achieve your online goals.
-				</CardDescription>
-			</CardHeader>
-			<form action={formAction}>
-				<CardContent className="flex flex-col gap-6 mt-2">
-					{state.success ? (
-						<p className="text-muted-foreground flex items-center gap-2 text-sm">
-							<CheckIcon className="size-4" />
-							Got It! Thanks for Reaching Out. I'll get back to you as soon as possible.
+		<Fragment>
+			<CardContent className="flex flex-col gap-6 mt-2">
+				{state.success ? (
+					<div className="flex flex-col items-center text-sm text-center">
+						<CheckCircleIcon className="size-12 text-green-500" />
+						<p className="mt-2">
+							Got It! Thanks for Reaching Out.
 						</p>
-					) : null}
-					<div className="group/field grid gap-2" data-invalid={!!state.errors?.name}>
-						<Label htmlFor="name" className="group-data-[invalid=true]/field:text-red-500 font-medium">
-							Name <span aria-hidden="true">*</span>
-						</Label>
-						<Input
-							id="name"
-							name="name"
-							placeholder="You name here"
-							className="group-data-[invalid=true]/field:border-destructive focus-visible:group-data-[invalid=true]/field:ring-destructive"
-							disabled={pending}
-							aria-invalid={!!state.errors?.name}
-							aria-errormessage="error-name"
-							defaultValue={state.defaultValues.name}
-						/>
-						{state.errors?.name && (
-							<p id="error-name" className="text-red-500 text-sm">
-								{state.errors.name}
-							</p>
-						)}
+						<p>
+							I'll get back to you as soon as possible.
+						</p>
 					</div>
-					<div className="group/field grid gap-2" data-invalid={!!state.errors?.email}>
-						<Label htmlFor="email" className="group-data-[invalid=true]/field:text-red-500 font-medium">
-							Email <span aria-hidden="true">*</span>
-						</Label>
-						<Input
-							id="email"
-							name="email"
-							placeholder="You email here"
-							className="group-data-[invalid=true]/field:border-destructive focus-visible:group-data-[invalid=true]/field:ring-destructive"
-							disabled={pending}
-							aria-invalid={!!state.errors?.email}
-							aria-errormessage="error-email"
-							defaultValue={state.defaultValues.email}
-						/>
-						{state.errors?.email && (
-							<p id="error-email" className="text-red-500 text-sm">
-								{state.errors.email}
-							</p>
-						)}
-					</div>
-					<div className="group/field grid gap-2" data-invalid={!!state.errors?.message}>
-						<Label htmlFor="message" className="group-data-[invalid=true]/field:text-red-500 font-medium">
-							Message <span aria-hidden="true">*</span>
-						</Label>
-						<Textarea
-							id="message"
-							name="message"
-							placeholder="Type your message here..."
-							className="group-data-[invalid=true]/field:border-destructive focus-visible:group-data-[invalid=true]/field:ring-destructive"
-							disabled={pending}
-							aria-invalid={!!state.errors?.message}
-							aria-errormessage="error-message"
-							defaultValue={state.defaultValues.message}
-						/>
-						{state.errors?.message && (
-							<p id="error-message" className="text-red-500 text-sm">
-								{state.errors.message}
-							</p>
-						)}
-					</div>
-				</CardContent>
+				) : null}
+				{!state.success && (
+					<Fragment>
+						<div className="group/field grid gap-2" data-invalid={!!state.errors?.name}>
+							<Label htmlFor="name" className="group-data-[invalid=true]/field:text-red-500 font-medium">
+								Name <span aria-hidden="true">*</span>
+							</Label>
+							<Input
+								id="name"
+								name="name"
+								placeholder="You name here"
+								className="group-data-[invalid=true]/field:border-destructive focus-visible:group-data-[invalid=true]/field:ring-destructive"
+								disabled={pending}
+								aria-invalid={!!state.errors?.name}
+								aria-errormessage="error-name"
+								defaultValue={state.defaultValues.name}
+							/>
+							{state.errors?.name && (
+								<p id="error-name" className="text-red-500 text-sm">
+									{state.errors.name}
+								</p>
+							)}
+						</div>
+						<div className="group/field grid gap-2" data-invalid={!!state.errors?.email}>
+							<Label htmlFor="email" className="group-data-[invalid=true]/field:text-red-500 font-medium">
+								Email <span aria-hidden="true">*</span>
+							</Label>
+							<Input
+								id="email"
+								name="email"
+								placeholder="You email here"
+								className="group-data-[invalid=true]/field:border-destructive focus-visible:group-data-[invalid=true]/field:ring-destructive"
+								disabled={pending}
+								aria-invalid={!!state.errors?.email}
+								aria-errormessage="error-email"
+								defaultValue={state.defaultValues.email}
+							/>
+							{state.errors?.email && (
+								<p id="error-email" className="text-red-500 text-sm">
+									{state.errors.email}
+								</p>
+							)}
+						</div>
+						<div className="group/field grid gap-2" data-invalid={!!state.errors?.message}>
+							<Label htmlFor="message" className="group-data-[invalid=true]/field:text-red-500 font-medium">
+								Message <span aria-hidden="true">*</span>
+							</Label>
+							<Textarea
+								id="message"
+								name="message"
+								placeholder="Type your message here..."
+								className="group-data-[invalid=true]/field:border-destructive focus-visible:group-data-[invalid=true]/field:ring-destructive"
+								disabled={pending}
+								aria-invalid={!!state.errors?.message}
+								aria-errormessage="error-message"
+								defaultValue={state.defaultValues.message}
+							/>
+							{state.errors?.message && (
+								<p id="error-message" className="text-red-500 text-sm">
+									{state.errors.message}
+								</p>
+							)}
+						</div>
+					</Fragment>
+				)}
+			</CardContent>
+			{!state.success && (
 				<CardFooter className="flex justify-center">
-					<Button type="submit" disabled={pending}>
+					<Button type="submit" disabled={pending} formAction={formAction}>
 						{pending ? "Sending..." : "Send Message"}
 					</Button>
 				</CardFooter>
-			</form >
-		</Card >
+			)}
+		</Fragment>
 	)
 }
+
+const ContactForm = () => (
+	<Card className="w-full max-w-lg mx-auto">
+		<CardHeader className="text-center">
+			<CardTitle>Let's build something great together.</CardTitle>
+			<CardDescription>
+				I'd love to hear about your project! Share your details, and we'll discuss how to achieve your online goals.
+			</CardDescription>
+		</CardHeader>
+		<form>
+			<ContactFormContent />
+		</form>
+	</Card >
+)
 
 export default ContactForm;
--- lib/actions.ts
+++ lib/actions.ts
@@ -2,6 +2,8 @@
 
 import {contactFormSchema} from "./schema"
 import {z} from "zod"
+import nodemailer from 'nodemailer';
+import Mail from "nodemailer/lib/mailer";
 
 export async function contactFormAction(_prevState: unknown, formData: FormData) {
 	const defaultValues = z.record(z.string(), z.string()).parse(Object.fromEntries(formData.entries()))
@@ -9,11 +11,45 @@ export async function contactFormAction(_prevState: unknown, formData: FormData)
 	try {
 		const data = contactFormSchema.parse(Object.fromEntries(formData))
 
-		// This simulates a slow response like a form submission.
-		// Replace this with your actual form submission logic.
-		await new Promise((resolve) => setTimeout(resolve, 1000))
+		const transport = nodemailer.createTransport({
+			service: 'gmail',
+			/*
+					setting service as 'gmail' is same as providing these setings:
 
-		console.log(data)
+					host: "smtp.gmail.com",
+					port: 465,
+					secure: true
+
+					If you want to use a different email provider other than gmail, you need to provide these manually.
+					Or you can go use these well known services and their settings at
+					https://github.com/nodemailer/nodemailer/blob/master/lib/well-known/services.json
+			*/
+			auth: {
+				user: process.env.GOOGLE_APP_USER,
+				pass: process.env.GOOGLE_APP_PASSWORD,
+			},
+		});
+
+		const mailOptions: Mail.Options = {
+			from: process.env.GOOGLE_APP_USER,
+			to: process.env.GOOGLE_APP_USER,
+			// cc: email, (uncomment this line if you want to send a copy to the sender)
+			subject: `Message from ${data.name}: (${data.email})`,
+			text: data.message,
+		};
+
+		const sendMailPromise = () =>
+			new Promise<string>((resolve, reject) => {
+				transport.sendMail(mailOptions, function (err) {
+					if (!err) {
+						resolve('Email sent');
+					} else {
+						reject(err.message);
+					}
+				});
+			});
+
+		await sendMailPromise();
 
 		return {
 			defaultValues: {
@@ -34,6 +70,9 @@ export async function contactFormAction(_prevState: unknown, formData: FormData)
 				),
 			}
 		}
+
+		// Handle unexpected errors
+		console.error("Unexpected error:", error);
 
 		return {
 			defaultValues,
