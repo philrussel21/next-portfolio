5c7852fba1475d94a43bc7086957036c75075e3f
Phil Antiporda <phil.antiporda21@gmail.com>
2025-06-07 23:08:28 +1000

    chore: fix status

--- components/molecules/profile-info.tsx
+++ components/molecules/profile-info.tsx
@@ -22,13 +22,13 @@ const ProfileInfo = ({isAvailable, stack, portrait}: ProfileInfoProperties): JSX
 				<h1 className="mt-4 heading-five !font-bold">Phil Antiporda</h1>
 				<Text size="small" className="text-center mt-2 max-w-sm mx-auto" content="Building fast, thoughtful websites with real-world impact."/>
 			</div>
-			<div className="mt-6 flex flex-col justify-center gap-2">
+			<div className="mt-6 flex justify-center gap-2">
 				{/* TODO: Uncomment to show location? */}
 				{/* <div className="flex justify-center items-baseline gap-1">
 					<MapPinIcon size={14}/>
 					<p className="text-sm">Australia</p>
 				</div> */}
-				<span className="flex items-center gap-4 bg-zinc-800/50 px-4 py-2 rounded-full justify-center">
+				<span className="flex items-center gap-4 bg-zinc-800/50 px-4 py-2 rounded-full justify-center grow-0">
 					<span className="relative flex h-3 w-3">
 						<span className={`animate-ping absolute inline-flex h-full w-full rounded-full opacity-75 ${isAvailable ? 'bg-green-400' : 'bg-red-400'}`}/>
 						<span className={`relative inline-flex rounded-full h-3 w-3 ${isAvailable ? 'bg-green-500' : 'bg-red-500'}`}/>
